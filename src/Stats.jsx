d18f935ed2150641da52ad45515ada870a100f66
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-08-07 19:42:09 +0530

    correct stats width

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,10 +1,7 @@
 function Stats() {
   return (
-    <div className=" w-[1340px] lg:max-w-[1531px] h-[373px] absolute top-[941px] left-[99px] rounded-[29px] border-[1px] pt-[82px] pb-[82px] gap-[10px] flex flex-col bg-[#0B0B0B] border-[#646464]">
-      <img
-        src="./stats.png"
-        className="w-[1000px] lg:w-[1532px] h-[209px] object-cover"
-      />
+    <div className=" max-w-[1531px] h-[373px] absolute top-[941px] left-[99px] rounded-[29px] border-[1px] pt-[82px] pb-[82px] gap-[10px] flex flex-col bg-[#0B0B0B] border-[#646464]">
+      <img src="./stats.png" className="w-[1532px] h-[209px] object-cover" />
     </div>
   );
 }
